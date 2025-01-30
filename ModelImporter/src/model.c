f6d91554d6d8c1b445d0dbbe9a6afbe2cde45e95
Rami <myersrami17@gmail.com>
2025-01-29 18:40:55 -0600

    OBJ parsing almost complete

--- ModelImporter/src/model.c
+++ ModelImporter/src/model.c
@@ -4,16 +4,16 @@
 #include <string.h>
 #include "model.h"
 
-// First pass of parser totals up number of each attribute to be allocated
-// TODO:
-// Second pass stores each attribute in its respective buffer
+// TODO: Debug seg fault after parsing texture coordinates
 // Send data to OpenGL
 // Draw model
 
 Model modelParse(const char *objFile) {
   Model model;
-  unsigned long nV = 0, nVT = 0, nVN = 0;
-  char line[LINE_LEN];
+  char c;
+  size_t vBytes = 10, vtBytes = 10, vnBytes = 10;
+  char attribute[3];
+  char values[LINE_LEN];
   char *linePtr, *currentChar;
 
   FILE *src = fopen(objFile, "r");
@@ -23,29 +23,70 @@ Model modelParse(const char *objFile) {
     exit(EXIT_FAILURE);
   }
 
-  // First Pass
-  while (fgets(line, LINE_LEN, src)) {
-    linePtr = line;
-    currentChar = linePtr;
-    while (*(currentChar++) != ' ');
-    *(currentChar - 1) = '\0';
-    if (!strcmp(linePtr, "v"))
-      nV++;
-    else if (!strcmp(linePtr, "vt"))
-      nVT++;
-    else if (!strcmp(linePtr, "vn"))
-      nVN++;
+  model.numVertices = 0;
+  model.numTextureCoords = 0;
+  model.numNormals = 0;
+
+  model.vertices = malloc(vBytes * sizeof(float));
+  model.textureCoords = malloc(vtBytes * sizeof(float));
+  model.normals = malloc(vnBytes * sizeof(float));
+
+  // Skips to the first attribute
+  while((c = fgetc(src)) != 'v');
+  ungetc(c, src);
+
+  while (1) {
+    // Reads to the first space to store the attribute type
+    currentChar = attribute;
+    while ((c = fgetc(src)) != ' ')
+      *(currentChar++) = c;   
+    *(currentChar) = '\0';
+
+    // Breaks if an invalid line is read
+    printf("Attribute: %s\n", attribute);
+    if (strcmp(attribute, "v") && strcmp(attribute, "vt") && strcmp(attribute, "vn")) {
+      printf("Breaking\n");
+      break;
+    }
+
+    // Vertex Coordinates
+    if (!strcmp(attribute, "v")) {
+      // Realloc
+      if (model.numVertices + 3 > vBytes) {
+        model.vertices = realloc(model.vertices, (vBytes + 10) * sizeof(float));
+        vBytes += 10;
+      }
+      fscanf(src, " %f", &model.vertices[model.numVertices++]);
+      fscanf(src, " %f", &model.vertices[model.numVertices++]);
+      fscanf(src, " %f\n", &model.vertices[model.numVertices++]);
+      printf("v: < %f, %f, %f >\n", model.vertices[model.numVertices - 3], model.vertices[model.numVertices - 2], model.vertices[model.numVertices - 1]);
+    }
+
+    // Normals
+    if (!strcmp(attribute, "vn")) {
+      // Realloc
+      if (model.numNormals + 3 > vnBytes) {
+        model.normals = realloc(model.normals, (vtBytes + 10) * sizeof(float));
+        vtBytes += 10;
+      }
+      fscanf(src, " %f", &model.normals[model.numNormals++]);
+      fscanf(src, " %f", &model.normals[model.numNormals++]);
+      fscanf(src, " %f\n", &model.normals[model.numNormals++]);
+      printf("vn: < %f, %f, %f >\n", model.normals[model.numNormals - 3], model.normals[model.numNormals - 2], model.normals[model.numNormals - 1]);
+    }
+
+    // Texture Coordinates
+    if (!strcmp(attribute, "vt")) {
+      // Realloc
+      if (model.numTextureCoords + 2 > vtBytes) {
+        model.textureCoords = realloc(model.textureCoords, (vtBytes + 10) * sizeof(float));
+        vtBytes += 10;
+      }
+      fscanf(src, " %f", &model.textureCoords[model.numTextureCoords++]);
+      fscanf(src, " %f\n", &model.textureCoords[model.numTextureCoords++]);
+      printf("vt: < %f, %f >\n", model.textureCoords[model.numTextureCoords - 2], model.textureCoords[model.numTextureCoords - 1]);
+    }
   }
-
-  // Allocate Buffer Memory
-  model.vertices = malloc(nV * sizeof(float));
-  model.textureCoords = malloc(nVT * sizeof(float));
-  model.normals = malloc(nVN * sizeof(float));
-
-  // Reset File Ptr
-  rewind(src);
-
-  // Second Pass
 
   return model;
 }
